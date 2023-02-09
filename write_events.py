08a6201f29de35415d372ff36e74964d60aa3f16
oferst13 <66622305+oferst13@users.noreply.github.com>
2023-02-09 16:53:34 +0200

    started event writer

--- write_events.py
+++ write_events.py
@@ -5,17 +5,30 @@ import os
 
 
 class Event:
+    all_events = []
+
     def __init__(self, first_idx, last_idx):
         self.first_idx = first_idx
         self.last_idx = last_idx
         self.name = None
         self.df = None
         self.meta_df = None
+        Event.all_events.append(self)
+
+
+def close_event(first, last):
+    event_array = np.concatenate([[0], rain_array[first:last + 1], [0]])
+    tot_mm = round(np.sum(event_array), 2)
+    duration = len(event_array) * rain_dt / 60
+    print(tot_mm, duration)
+    print('')
+
+#def find_next_ze
 
 
 raw_path = 'rain_files'
 df_path = raw_path + '/df_rain_files'
-diff_event = 6  # difference between event in hours
+diff_event = 6  # min difference between events in hours
 season = '09-10'
 rain_file = season + '.csv'
 rain_df_file = df_path + '/' + season + '-df.csv'
@@ -43,4 +56,19 @@ rain_dt = (season_data.iloc[1, 0] - season_data.iloc[0, 0]).total_seconds() / 60
 rain_header = list(season_data)[1]
 rain_array = season_data[rain_header].to_numpy()
 first_rain = np.min(np.nonzero(rain_array))
+last_rain = np.max(np.nonzero(rain_array))
+i = first_rain
+while i <= last_rain:
+    i = i + np.min(np.nonzero(rain_array[i:last_rain]))
+    first_i = i
+    i = i + np.min(np.nonzero(rain_array[i:last_rain] == 0))
+    next_diff = np.sum(rain_array[i:i + int(diff_event * (60 / rain_dt))])
+    while next_diff:
+        i = i + np.max(np.nonzero(rain_array[i:i + int(diff_event * (60 / rain_dt))]))
+        next_diff = np.sum(rain_array[i + 1:i + int(diff_event * (60 / rain_dt))])
+    last_i = i
+    i += 1
+    close_event(first_i, last_i)
+
+
 print(' ')
