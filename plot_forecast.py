cd7ff54e07d54cdccda10a45d15d2e1d21e5108f
oferst13 <66622305+oferst13@users.noreply.github.com>
2023-05-14 12:54:49 +0300

    compare forecast

--- plot_forecast.py
+++ plot_forecast.py
@@ -3,15 +3,29 @@ import cfg
 from matplotlib import pyplot as plt
 import numpy as np
 
+
+def set_forecast_idx(first, last, diff):
+    return np.arange(first, last, diff)
+
+
 rain_path = 'rain_files/df_rain_files/df_events'
 forecast_path = 'rain_files/Forecasts/'
-file = cfg.files[0]
-event_df = pd.read_csv(file, index_col=False)
-rain_header = list(event_df)[1]
-rain_array = event_df[rain_header].to_numpy()
-event_dates = file.split('\\')[1].split('.')[0]
-forecast_df = pd.read_csv(forecast_path + event_dates + '-swap.csv')
-forecast_array = forecast_df[rain_header].to_numpy()
+for file in cfg.files:
+    for suff in ['plusMin', 'swap']:
+        forecast_mode = '-' + suff + '.csv'
+        event_df = pd.read_csv(file, index_col=False)
+        rain_header = list(event_df)[1]
+        rain_array = event_df[rain_header].to_numpy()
+        event_dates = file.split('\\')[1].split('.')[0]
+        forecast_array = np.genfromtxt(forecast_path + event_dates + forecast_mode, delimiter=',')
+        num_forecasts = len(forecast_array)
+        forecast_indices = set_forecast_idx(0, num_forecasts, int(cfg.sample_interval / cfg.forecast_interval))
+        to_compare = np.zeros((len(forecast_indices), len(forecast_array[0, :]), 2))
+        for target_idx, idx in enumerate(forecast_indices):
+            to_compare[target_idx,:,0] = forecast_array[idx,:]
+            start_idx_rain = int(target_idx * (cfg.sample_interval / cfg.rain_dt))
+            rain_to_write = rain_array[start_idx_rain:start_idx_rain+18]
+            to_compare[target_idx,:len(rain_to_write), 1] = rain_to_write
 rain_hours = np.linspace(0, int(cfg.sim_days * 24), int(cfg.sim_days * 24 * 3600 / cfg.rain_dt) + 1,
-                             dtype='longfloat')
+                         dtype='longfloat')
 print(' ')
