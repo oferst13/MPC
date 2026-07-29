ae7bcddf3e26d65cf7e5e03760db2468434a682b
oferst13 <66622305+oferst13@users.noreply.github.com>
2026-07-29 14:22:31 +0300

    Preserve current experiment configuration and ignore runtime files

--- plot_results.py
+++ plot_results.py
@@ -6,14 +6,14 @@ plt.rcParams["figure.figsize"] = [9, 6]
 
 results_df = pd.read_csv('results-mae.csv', index_col=False)
 results_df['Mean_intens'] = results_df['Total_mm'] / results_df['Duration']
-flow_compare = True
+flow_compare = False
 if flow_compare:
     q_high = results_df['Flow_from_perfect-swap'].quantile(0.999)
     filtered_df = results_df[(results_df['Flow_from_perfect-swap'] < q_high)]
     filtered_df['Flow_from_perfect-swap'] = filtered_df['Flow_from_perfect-swap']
     filtered_df['Flow_from_perfect-plusMin'] = filtered_df['Flow_from_perfect-plusMin']
-#ax = results_df.plot(x='Flow_reduction-perfect', y='Water_reduction-perfect', kind='scatter', marker='o', color='xkcd:dark sky blue', label='Perfect', s=40)
-boxplot = False
+# ax = results_df.plot(x='Total_mm', y='Water_reduction-perfect', kind='scatter', marker='o', color='xkcd:dark sky blue', label='Perfect', s=40)
+boxplot = True
 histo = False
 if boxplot:
     plt.rcParams["figure.figsize"] = [4, 6]
@@ -22,22 +22,26 @@ if boxplot:
     capsprops = dict(linewidth=3)
     medianprops = dict(linewidth=3, color='g')
     meanpointprops = dict(markersize=7, marker='D')
-    fig = results_df.boxplot(column=['Water_from_perfect-plusMin', 'Water_from_perfect-swap'],
+    fig = results_df.boxplot(column=['Water_reduction-perfect','Water_reduction-plusMin', 'Water_reduction-swap'],
                              grid=False, meanline=False, showmeans=True, boxprops=boxprops, whiskerprops=whiskerprops,
                              medianprops=medianprops, capprops=capsprops, meanprops=meanpointprops)
 else:
-    ax = filtered_df.plot(x='MAE-plusMin', y='Flow_from_perfect-plusMin', kind='scatter', marker=(5,1), color='xkcd:bright orange', label='Depth',s=35)
-    #results_df.plot(x='Water_reduction-plusMin', y='Water_reduction-plusMin', kind='scatter', marker=(5,1), ax=ax,
-                    #color='xkcd:bright orange', label='Depth', s=35)
-    filtered_df.plot(x='MAE-swap', y='Flow_from_perfect-swap', kind='scatter', marker='+',ax=ax, color='xkcd:lightish green', label='Depth & swap',s=30)
+    # ax = filtered_df.plot(x='MAE-plusMin', y='Flow_from_perfect-plusMin', kind='scatter', marker=(5,1), color='xkcd:bright orange', label='Depth',s=35)
+    results_df.plot(x='Total_mm', y='Water_reduction-plusMin', kind='scatter', marker=(5,1), ax=ax,
+                    color='xkcd:bright orange', label='Depth', s=35)
+    results_df.plot(x='Total_mm', y='Water_reduction-swap', kind='scatter', marker='+',ax=ax, color='xkcd:lightish green', label='Depth & swap',s=30)
     ax.legend(title='Forecast type', fontsize=11)
-plt.xlabel('Max EDf5 ' + r'$(\frac{mm}{hr})$', fontsize=12)
-#plt.xlabel('Forecast type', fontsize=12)
-#plt.ylabel('Water availability reduction %', fontsize=12)
-plt.ylabel(r'$\Delta$WR %', fontsize=12)
-#plt.xticks([1,2], ['Depth','Depth & swap'], fontsize=11)
-plt.xticks(fontsize=11)
+# plt.xlabel('Max EDf5 ' + r'$(\frac{mm}{hr})$', fontsize=12)
+#plt.xlabel('Event depth (mm)', fontsize=12)
+plt.xlabel('Forecast type', fontsize=12)
+plt.ylabel('Water availability reduction %', fontsize=12)
+#plt.ylabel(r'$\Delta$WR %', fontsize=12)
+plt.xticks([1,2,3], ['Perfect','Depth','Depth & swap'], fontsize=11)
+#plt.xticks(fontsize=11)
 plt.yticks(fontsize=11)
+if boxplot:
+    ax = plt.gca()
+ax.invert_yaxis()
 plt.show()
 
 print('end')
